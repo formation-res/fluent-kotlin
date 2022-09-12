07ffab5fb3fd3fec50a8cbbf541f6a7c195f3489
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2022-09-12 11:30:48 +0200

    cleanup

--- src/jvmMain/kotlin/com/tryformation/localization/LocalizedTranslationBundleSequenceProvider.kt
+++ src/jvmMain/kotlin/com/tryformation/localization/LocalizedTranslationBundleSequenceProvider.kt
@@ -6,30 +6,6 @@ import fluent.syntax.parser.FTLParser
 import fluent.syntax.parser.FTLStream
 import java.util.Locale
 
-//class SimpleFluentBasedBundle(override val locale: List<String>, val translationMap: Map<String, String>) :
-//    LocalizedTranslationBundle {
-//    override fun translate(translatable: Translatable, args: Map<String, Any>?): String? {
-//        return translationMap[translatable.messageId]
-//    }
-//
-//    companion object {
-//        fun fromString(locales: List<String>, source: String): SimpleFluentBasedBundle {
-//            val translationMap = source.lines().filter {
-//                val trimmed = it.trim()
-//                trimmed.isNotBlank() && !trimmed.startsWith("#")
-//            }.mapNotNull { line ->
-//                val index = line.indexOf('=')
-//                if (index > 0) {
-//                    line.substring(0, index).trim() to line.substring(index).trim()
-//                } else {
-//                    null
-//                }
-//            }.toMap()
-//            return SimpleFluentBasedBundle(locales, translationMap)
-//        }
-//    }
-//}
-
 class JavaFluentAdapter(override val locale: List<String>, private val ftlContent: String) :
     LocalizedTranslationBundle {
     private val bundle: FluentBundle
@@ -60,7 +36,6 @@ actual class LocalizedTranslationBundleSequenceProvider {
         val bundles = (locales + fallbackLocale).mapNotNull { locale ->
             fetch.invoke(locale)?.let { source ->
                 JavaFluentAdapter(listOf(locale),source)
-//                SimpleFluentBasedBundle.fromString(listOf(locale), source)
             }
         }
         return LocalizedTranslationBundleSequence(bundles)
