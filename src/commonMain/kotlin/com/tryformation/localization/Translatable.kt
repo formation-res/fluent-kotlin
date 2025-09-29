b5fc7a82c6c01dff276242225e7fadc85527f2e6
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2025-09-29 13:06:18 +0200

    use humanReadable

--- src/commonMain/kotlin/com/tryformation/localization/Translatable.kt
+++ src/commonMain/kotlin/com/tryformation/localization/Translatable.kt
@@ -9,9 +9,22 @@ private fun String.camelCase2SnakeCase(): String {
     return re.replace(this) { m -> "${m.groups[1]?.value}_${m.groups[2]?.value}" }.lowercase()
 }
 
+fun String.humanReadable(): String {
+    return this.replace(Regex("([a-z])([A-Z])"), "$1 $2") // Handle camelCase
+        .replace(Regex("[-_]"), " ") // Replace - or _ with space
+        .lowercase() // Convert to lowercase
+        .replaceFirstChar { it.uppercase() } // Capitalize the first letter
+}
+
 interface Translatable {
+    /** Name of the translatable. Note, if you are using enums, this would be the enum value's name. */
     val name: String
+    /** Prefix used in your fluent translation files */
     val prefix: String
+    /**
+     * Prefix with your enum value. Note, the default
+     * implementation takes care of lower casing and converting camel case to snake case for you.
+     */
     val messageId: String
         get() = "$prefix-" + (name.let { s ->
             // only apply conversion if it has lowercase letters
@@ -23,15 +36,10 @@ interface Translatable {
         }).replace('_', '-').lowercase()
 
     /**
-     * Falback if no translation is found.
+     * Falback if no translation is found. The default simply
      */
     val defaultTranslation: String
-        get() {
-            val postFix = messageId.replace("$prefix-", "")
-
-            return postFix.split('-') // Split on CamelCase or underscore
-                .joinToString(" ") { it.lowercase().replaceFirstChar { char -> char.uppercase() } }
-        }
+        get() = name.humanReadable()
 }
 
 /**
